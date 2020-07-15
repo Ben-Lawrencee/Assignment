6d418f6921c91e7071ff34cae886c6de3af54640
Ben Lawrence <1enderzen1@gmail.com>
2020-07-15 17:31:00 +1200

    Binary Tree Completed

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -1,69 +1,83 @@
 using System;
-using System.IO;
 using System.Collections.Generic;
 namespace Assignment {
   public class BinaryTree {
     private BinaryTreeNode topNode;
-    public BinaryTree() {
-      String text = "";
-      Random rand = new Random();
-      for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 20; j++) {
-          text = text + rand.Next(1, 21) + "\n";
-        }
-        File.WriteAllText("./numbers" + (i + 1) + ".txt", text);
-      }
-      List<int> numbers = new List<int>();
-      for (int i = 0; i < 3; i++) {
-        foreach (String j in File.ReadAllLines("./numbers" + (i + 1) + ".txt")) {
-          try {
-            if (j.Length == 0) continue;
-            numbers.Add(int.Parse(j));
-          } catch {
-            Console.WriteLine("Found NaN: \"{0}\"", j);
-            continue;
-          }
-        }
-      }
-      topNode = new BinaryTreeNode(numbers[(int)Math.Floor(Math.Round((decimal)numbers.Count / 2))], null, null);
+    public BinaryTree(int[] numbers) {
+      topNode = new BinaryTreeNode(numbers[(int)Math.Floor(Math.Round((decimal)numbers.Length / 2))], null, null);
       foreach (int i in numbers) {
         add(i, topNode);
       }
       Console.WriteLine("Binary Tree Created");
+    }
+    public void displayTree() {
       List<BinaryTreeNode> List = new List<BinaryTreeNode>();
       List.Add(topNode);
-      displayTree(List, 0);
+      recurseDisplayTree(List, 0);
     }
-    public void displayTree(List<BinaryTreeNode> list, int depth) {
+    public void displayPrimes() {
+      List<BinaryTreeNode> List = new List<BinaryTreeNode>();
+      List.Add(topNode);
+      recurseDisplayPrimes(List, 0);
+    }
+    private void recurseDisplayPrimes(List<BinaryTreeNode> list, int depth) {
+      int count = list.Count;
+      BinaryTreeNode node;
+      if (count == 0) return;
+      if (depth == 0) Console.WriteLine("Displaying primes by layer: ");
+      int primeCount = 0;
+      for (int i = 0; i < count; i++) {
+        node = list[0];
+        list.RemoveAt(0);
+        if (node.value >= 2 && isPrime(node.value)) {
+          if (primeCount == 0) Console.Write("[");
+          else Console.Write(",");
+          Console.Write("{0}", node.value);
+          primeCount++;
+        }
+        if (node.left != null) list.Add(node.left);
+        if (node.right != null) list.Add(node.right);
+      }
+      if (primeCount > 0) Console.Write("]");
+      recurseDisplayPrimes(list, depth + 1);
+    }
+    private Boolean isPrime(int n) {
+      Boolean b = true;
+      int max = (int)Math.Sqrt((double)n);
+      for (int i = 2; i <= max; i++) {
+        if (n % i == 0) {
+          b = false;
+          break;
+        }
+      }
+      return b;
+    }
+    private void recurseDisplayTree(List<BinaryTreeNode> list, int depth) {
       int count = list.Count;
       if (count == 0) return;
-      Console.Write("Layer: {0} [", depth);
+      if (depth == 0) Console.Write("Root: ");
+      else Console.Write("Layer: {0} [", depth);
       BinaryTreeNode node;
       for(int i = 0; i < count; i++) {
         node = list[0];
         list.RemoveAt(0);
         Console.Write("{0}", node.value);
-        if (i != count - 1) Console.Write(", ");
+        if (i != count - 1) Console.Write(",");
         if (node.left != null) list.Add(node.left);
         if (node.right != null) list.Add(node.right);
       }
-      Console.Write("]\n");
-      displayTree(list, depth + 1);
+      if (depth > 0) Console.Write("]\n");
+      else Console.Write("\n");
+      recurseDisplayTree(list, depth + 1);
     }
     public void add(int num, BinaryTreeNode node) {
       if (num == node.value) return; //If number already exists. Return
-      int temp = 0;
       if (num > node.value) {
-        if (num > node.right.value) { //If number is greater than right. Make new right and connect that right with prevs rights right.
-          temp = node.right.value;
-          node.right.value = num;
-          if (node.right.right == null) node.right.setRight(new BinaryTreeNode(temp, node.right.left, node.right.right));
-          else {
-
-          }
-        } else {
-          
-        }
+        if (node.right == null) node.setRight(new BinaryTreeNode(num, null, null));
+        else add(num, node.right);
+      } else {
+        if (node.left == null) node.setLeft(new BinaryTreeNode(num, null, null));
+        else add(num, node.left);
       }
     }
   }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-
+using System.IO;
 
 namespace Assignment {
   class Program {
@@ -8,7 +7,36 @@ namespace Assignment {
       Console.Clear();
       ArrayTask.run();
       LinkedList.run();
-      BinaryTree tree = new BinaryTree();
+
+      String text = "";
+      Random rand = new Random();
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 20; j++) {
+          text = text + rand.Next(1, 21) + "\n";
+        }
+        File.WriteAllText("./numbers" + (i + 1) + ".txt", text);
+      }
+      int[] numbers;
+      int index = 0;
+      String[] file;
+      for (int i = 0; i < 3; i++) {
+        file = File.ReadAllLines("./numbers" + (i + 1) + ".txt");
+        numbers = new int[file.Length];
+        foreach (String j in file) {
+          try {
+            if (j.Length == 0) continue;
+            numbers[index] = int.Parse(j);
+          } catch {
+            Console.WriteLine("Character was not a number: \"{0}\"", j);
+            continue;
+          }
+          index++;
+        }
+        index = 0;
+        BinaryTree tree = new BinaryTree(numbers);
+        tree.displayTree();
+        tree.displayPrimes();
+      }
     }
     public static Boolean isPrime(int n) {
       Boolean b = true;
